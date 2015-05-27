fe92e71882c71933ec2b4aa040d12787a932da57
Mac <Sang47@MacBook-Air-Bonachev.local>
2015-05-27 21:42:57 +0500

    Player and enemy
    Добавлен игрок и  1 противник
    Они движутся

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	// 1 - переменные
+	
+	/// <summary>
+	/// Скорость объекта
+	/// </summary>
+	public Vector2 speed = new Vector2(10, 10);
+	
+	/// <summary>
+	/// Направление движения
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+	
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 2 - Перемещение
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+
+	}
+
+	void FixedUpdate()
+	{
+		// Применить движение к Rigidbody
+		Rigidbody2D rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+		rigidBody2D.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	/// 1 - скорость движения
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - направление движения
+	private Vector2 movement;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 3 -  извлечь информацию оси
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - движение в каждом направлении
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		// 5 - перемещение игрового объекта
+		//Rigidbody2D rigidBody2D;
+		//rigidBody2D = gameObject.GetComponent<"Rigidbody2D">();
+		Rigidbody2D rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+		rigidBody2D.velocity = movement;
+	}
+}
